b38da03fc6d5fbcb3b40ec80a830aee4420bfa10
wholesome-bean <rdcarter97@gmail.com>
2023-04-19 17:46:05 -0400

    Idle/Run animations for player

--- main.cpp
+++ main.cpp
@@ -21,6 +21,17 @@ int main()
         (float)windowDimensions[0]/2.0f - 4.0f * (0.5 * (float)knight.width)/6.0,
         (float)windowDimensions[1]/2.0f - 4.0f * (0.5 * (float)knight.height)
     };
+    // 1 : facing right -1 : facing left
+    float rightLeft{1.f};
+
+    Texture2D knight_idle = LoadTexture("characters/knight_idle_spritesheet.png");
+    Texture2D knight_run = LoadTexture("characters/knight_run_spritesheet.png");
+
+    // Animation Variables
+    float runningTime{};
+    int frame{};
+    const int maxFrames{6};
+    const float updateTime{1.f/12.f};
 
     while (!WindowShouldClose())
     {
@@ -38,14 +49,26 @@ int main()
         {
             // Normalize to avoid moving faster diagonally
             mapPos = (Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed)));
-        }
+            direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f;
+            knight = knight_run;
+        } else knight = knight_idle;
         
         // Draw the map
         DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
 
+        // Update animation frame
+        runningTime += GetFrameTime();
+        if (runningTime >= updateTime)
+        {
+            frame++;
+            runningTime = 0.f;
+            if(frame > maxFrames) frame = 0;
+        }
+
         // Draw the knight
-        Rectangle source{0.f, 0.f, (float)knight.width/6.f, (float)knight.height};
-        Rectangle dest{knightPos.x, knightPos.y, 4.0f * (float)knight.width/6.0, 4.0f * (float)knight.height};
+        // source selects which animation frame to use from sprite sheet
+        Rectangle source{frame * (float)knight.width/6.f, 0.f, rightLeft * (float)knight.width/6.f, (float)knight.height};
+        Rectangle dest{knightPos.x, knightPos.y, 4.0f * (float)knight.width/6.f, 4.f * (float)knight.height};
         DrawTexturePro(knight, source, dest, Vector2{}, 0.f, WHITE);
 
 
